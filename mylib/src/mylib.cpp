e553c6371405aef9115ef41c690c87954793b9ba
Ruben Perez <rubenperez038@gmail.com>
2024-04-16 15:01:53 +0200

    include std

--- mylib/src/mylib.cpp
+++ mylib/src/mylib.cpp
@@ -1,8 +1,10 @@
 
 #include "mylib_iface.hpp"
 #include <iostream>
+#include <ratio>
+#include <chrono>
 
 void mylib::g()
 {
-    std::cout << "g()\n";
+    std::cout << "g(): " << std::chrono::system_clock::now().time_since_epoch().count() << std::endl;
 }
