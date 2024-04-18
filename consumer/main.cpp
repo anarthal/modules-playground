79026162612a62f6fa9e2079a810d5d4f991a34e
Ruben Perez <rubenperez038@gmail.com>
2024-04-18 12:25:52 +0200

    charconv consumer

--- consumer/main.cpp
+++ consumer/main.cpp
@@ -1,12 +1,11 @@
-import asio;
+import charconv;
 import std;
-import mylib;
 
 
 int main()
 {
-    asio::io_context ctx;
-    ctx.run();
-    mylib::f();
-    mylib::g();
+    char buff [512]{};
+    auto [ptr, ec] = boost::charconv::to_chars(buff, buff+sizeof(buff), 4.2);
+    std::cout << "size: " << (ptr-buff) << ", ec: " << std::make_error_code(ec) << std::endl;
+    std::puts(buff);
 }
